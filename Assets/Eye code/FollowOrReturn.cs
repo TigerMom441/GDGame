5ea5e898df6364371a3442eaceb5ae2429cb4fcb
TigerMom441 <gordwill123@gmail.com>
2023-12-06 20:53:33 -0500

    updated game play
    Got everything set up, jsut need to mess around with it a bit more. We also need to figure out how you win the game

---
+++ Assets/Eye code/FollowOrReturn.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowOrReturn : MonoBehaviour
+{
+    //Where the enemy returns too
+    public Transform HomeBase;
+    //The player
+    public Transform Player;
+    //The enemy speed
+    public float speed = 1.0f;
+
+    private Transform target;
+    private Rigidbody2D rb;
+    private bool _stop = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Grab the rb
+        rb = GetComponent<Rigidbody2D>();
+        //set player as Target from the the start
+        targetPlayer();
+    }
+
+    // FixedUpdate is called after each physics calculations
+    private void FixedUpdate()
+    {
+        //If the stop boolean is false, chase whichever target is set
+        if (!_stop)
+        {
+            rb.MovePosition(Vector2.Lerp(transform.position, target.position, Time.fixedDeltaTime * speed));
+        }
+
+    }
+
+    //Target the player
+    public void targetPlayer()
+    {
+        target = Player;
+    }
+
+    //Target the Home Base
+    public void targetBase()
+    {
+        target = HomeBase;
+    }
+
+    //Switch enemy movement on or off
+    public void stopPlayEnemy()
+    {
+        _stop = !_stop;
+    }
+
+}
